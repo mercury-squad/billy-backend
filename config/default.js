afab052f51629fd7cfd846b8500175475128e717
Ashim Karki <aceem123@gmail.com>
2022-06-12 22:55:52 -0700

    enabled the SMTP and email service for backend and added template for emails

--- config/default.js
+++ config/default.js
@@ -9,9 +9,6 @@
  * @version     1.0
  */
 
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
   LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
   HOST_URL: process.env.HOST || 'http://localhost:3200',
@@ -22,14 +19,21 @@ module.exports = {
     (process.env.PASSWORD_HASH_SALT_LENGTH && Number(process.env.PASSWORD_HASH_SALT_LENGTH)) || 10,
   JWT_SECRET: process.env.JWT_SECRET || 'hjijfvbw859',
   JWT_EXPIRATION: process.env.JWT_EXPIRATION || '100 days',
+  emailVerificationContent: `Dear %s<br/> <br/>
+  Thank you for signing up to Billy. Please click on this <a href="%s">link</a> which will validate the email address 
+  that you used to register. Once you click on this link you are ready to log in. We look forward to talking with you soon.
+  <br/> <br/>If any questions or problems, please call (000) 000-0000 Or email to support@billy.com`,
+  forgotPasswordContent: `Dear %s<br/> <br/>
+  Please click on this <a href="%s">link</a> which will guide you through the process of changing the password.
+  <br/> <br/>If any questions or problems, please call (000) 000-0000 Or email to support@billy.com`,
   email: {
     pool: true,
     host: process.env.EMAIL_SERVER || 'smtp.gmail.com',
     port: process.env.EMAIL_PORT || 465,
     secure: true, // use TLS
     auth: {
-      user: process.env.EMAIL_USER || 'usertotestmail@gmail.com',
-      pass: process.env.EMAIL_PASS || 'UserTest@1234',
+      user: process.env.EMAIL_USER || 'invoicebilly@gmail.com',
+      pass: process.env.EMAIL_PASS || 'sullfvzajfodtjry',
     },
     debug: true, // include SMTP traffic in the logs
   },
