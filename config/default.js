6632b0eed2505009489390b09c0b315efe1bb681
Ashim Karki <aceem123@gmail.com>
2022-06-12 23:49:38 -0700

    removed sensitive info into envs

--- config/default.js
+++ config/default.js
@@ -10,15 +10,16 @@
  */
 
 module.exports = {
-  LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
+  LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   HOST_URL: process.env.HOST || 'http://localhost:3200',
-  FRONTEND_URL: process.env.FRONTEND || 'http://localhost:4200',
+  FRONTEND_URL: process.env.FRONTEND || 'http://localhost:3000',
   PORT: process.env.PORT || 3200,
   API_VERSION: process.env.API_VERSION || '/api/v1',
   PASSWORD_HASH_SALT_LENGTH:
     (process.env.PASSWORD_HASH_SALT_LENGTH && Number(process.env.PASSWORD_HASH_SALT_LENGTH)) || 10,
   JWT_SECRET: process.env.JWT_SECRET || 'hjijfvbw859',
   JWT_EXPIRATION: process.env.JWT_EXPIRATION || '100 days',
+
   emailVerificationContent: `Dear %s<br/> <br/>
   Thank you for signing up to Billy. Please click on this <a href="%s">link</a> which will validate the email address 
   that you used to register. Once you click on this link you are ready to log in. We look forward to talking with you soon.
@@ -26,14 +27,15 @@ module.exports = {
   forgotPasswordContent: `Dear %s<br/> <br/>
   Please click on this <a href="%s">link</a> which will guide you through the process of changing the password.
   <br/> <br/>If any questions or problems, please call (000) 000-0000 Or email to support@billy.com`,
+
   email: {
     pool: true,
-    host: process.env.EMAIL_SERVER || 'smtp.gmail.com',
-    port: process.env.EMAIL_PORT || 465,
+    host: process.env.EMAIL_SERVER,
+    port: process.env.EMAIL_PORT,
     secure: true, // use TLS
     auth: {
-      user: process.env.EMAIL_USER || 'invoicebilly@gmail.com',
-      pass: process.env.EMAIL_PASS || 'sullfvzajfodtjry',
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
     debug: true, // include SMTP traffic in the logs
   },
